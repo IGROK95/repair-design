41f33408823f03903808f8784ea32e93cb55fb4a
IGROK95 <igrok95_13@mail.ru>
2020-03-04 23:17:20 +0300

    create cssMinfy

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const cssMin = require('gulp-css');
 const rename = require("gulp-rename");
 
 gulp.task('server', function() {
@@ -16,18 +14,9 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+gulp.task('cssMinfy', function(){
+  return gulp.src('src/**/*.css')
+    .pipe(cssMin())
+    .pipe(rename({suffix: '.min', prefix: ''}))
+    .pipe(gulp.dest('src'));
 });
-
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
