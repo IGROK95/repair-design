56cd25a7325cbb86ae31ed548c9a669811dc2729
IGROK95 <igrok95_13@mail.ru>
2020-03-15 12:24:07 +0300

    feat: rewrote JS code in Jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+/*document.addEventListener('DOMContentLoaded', function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -26,4 +26,35 @@ document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('.modal.modal--visible').classList.remove('modal--visible');
     this.classList.remove('modal--visible');
 });
+});
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    modalDialog = $('.modal__dialog')
+    closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  modal.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  modalDialog.on('click', function (e) {
+    e.stopPropagation();
+  });
+
+  $(document).keyup(function(event){
+    if(event.which=='27'){
+        $('.modal').removeClass('modal--visible');
+    }
+ });
+
 });
